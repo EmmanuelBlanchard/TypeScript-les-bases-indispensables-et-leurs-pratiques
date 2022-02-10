86f2b5fcfe1595edd465023d823c15da36e35465
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-02-10 21:35:47 +0100

    02-Module2-LesBasesDeTypeScript-03-LesTypesDeBaseAvecTypeScripts

---
+++ Programmes/02-Module2-LesBasesDeTypeScript/03-LesTypesDeBaseAvecTypeScript/main.ts
@@ -0,0 +1,33 @@
+let firstName = "Matthieu";
+let age = 32;
+let sexe = true;
+const sports = ["foot","rugby"];
+const address = {
+    line : "rue des fleurs",
+    city : "Toulouse",
+    zipCode : 31000
+}
+
+let afficherPersonne = (
+    in_first_name:string, 
+    in_age:number, 
+    in_sexe:boolean, 
+    in_sports:string[], 
+    in_address:{
+        line : string;
+        city : string;
+        zipCode : number;
+    }
+    ) => {
+    console.log(`Prenom : ${in_first_name}`);
+    console.log("Age : " + in_age);
+    console.log("Sexe : " + (in_sexe ? "Homme" : "Femme"));
+    for(let sport of in_sports) {
+        console.log(sport)
+    }
+    console.log("Adresse : ");
+    console.log(in_address.line)
+    console.log(in_address.zipCode + " " + in_address.city);
+}
+
+afficherPersonne(firstName,age,sexe,sports,address);
