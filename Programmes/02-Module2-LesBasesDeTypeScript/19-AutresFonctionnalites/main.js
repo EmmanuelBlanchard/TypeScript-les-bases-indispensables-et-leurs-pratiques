cb3feb43e16eab29ed52189691f1f1b61c74a908
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-02-11 22:06:43 +0100

    02-Module2-LesBasesDeTypeScript-19-AutresFonctionnalites

---
+++ Programmes/02-Module2-LesBasesDeTypeScript/19-AutresFonctionnalites/main.js
@@ -0,0 +1,5 @@
+var inputNom = document.querySelector("#nom");
+inputNom.value = "test";
+var inputAge = document.querySelector("#age");
+inputAge.value = "31";
+var titreH1 = document.querySelector("#titre1");
---
+++ Programmes/02-Module2-LesBasesDeTypeScript/19-AutresFonctionnalites/main.ts
@@ -0,0 +1,6 @@
+const inputNom = document.querySelector("#nom")! as HTMLInputElement;
+inputNom.value="test";
+const inputAge = document.querySelector("#age")! as HTMLInputElement;
+inputAge.value="31";
+
+const titreH1 = document.querySelector("#titre1") as HTMLHeadingElement;
