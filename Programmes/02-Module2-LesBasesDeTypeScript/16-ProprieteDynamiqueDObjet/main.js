999eb28179a4f013aebcf7af5ec2d2781a6406e4
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-02-11 18:21:03 +0100

    02-Module2-LesBasesDeTypeScript-16-ProprieteDynamiqueDObjet

---
+++ Programmes/02-Module2-LesBasesDeTypeScript/16-ProprieteDynamiqueDObjet/main.js
@@ -0,0 +1,34 @@
+var p1 = {
+    name: "Matthieu",
+    age: 31,
+    weight: 60
+};
+p1.size = 180;
+/*
+    Typage de propriétés
+
+    => Lorsque l'on définit un objet par l'intermédiaire du mot clé << type >>
+    ou via une interface, il est possible de définir le type de propriété que pourra
+    contenir l'objet.
+
+    => Exemple :
+
+    type erreur = {
+        [props:string] : string;  // Les objets de type << erreur >> pourront avoir autant de propriétés que nécessaires mais toutes du type << string >>.
+        code : string;
+    }
+
+    => Exemple d'utilisation :
+
+    const page404:erreur = {
+        code: "404",
+        message : "La page n'existe pas",
+        image : "page404.jpg",
+    }
+
+    const page500:erreur = {
+        code: "500",
+        message : "Erreur serveur"
+    }
+
+*/ 
