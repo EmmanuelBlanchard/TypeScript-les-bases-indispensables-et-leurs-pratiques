d65509cdc3d305705e0e6378bb78b17be344c12e
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-02-14 23:00:03 +0100

    05-Module05-Projet2-DevinerLeDrapeauDUnPays(utilisationDeAPIRestCountries)-02-LaRecuperationDesDonneesAvecAPIRestCountries

---
+++ Programmes/05-Module05-Projet2-DevinerLeDrapeauDUnPays(utilisationDeAPIRestCountries)/02-LaRecuperationDesDonneesAvecAPIRestCountries/dist/main.js
@@ -0,0 +1,13 @@
+"use strict";
+fetch("https://restcountries.com/v2/all").then(function (response) {
+    var contentType = response.headers.get("content-type");
+    if (contentType && contentType.indexOf("application/json") !== -1) {
+        return response.json().then(function (json) {
+            console.log(json);
+        });
+    }
+    else {
+        document.querySelector("h1").textContent = "Les informations récupérées ne sont pas au format JSON";
+    }
+});
+//# sourceMappingURL=main.js.map
