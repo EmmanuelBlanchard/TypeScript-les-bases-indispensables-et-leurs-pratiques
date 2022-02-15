0356aec5e4eba9e5c2bc9aacde93d6c365b19639
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-02-15 16:56:36 +0100

    06-Module6-LaProgrammationOrienteeObjet(POO)-01-RappelsDePOOEtDeJS

---
+++ Programmes/06-Module6-LaProgrammationOrientéeObjet(POO)/01-RappelsDePOOEtDeJS/main.ts
@@ -0,0 +1,27 @@
+class Car {
+    brand;
+    model;
+    color;
+    numberOfDoors;
+
+    constructor(brand,model,color,numberOfDoors) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    showCar() {
+        console.log("Marque : " + this.brand);
+        console.log("Modèle : " + this.model);
+        console.log("Couleur : " + this.color);
+        console.log("Nombre de portes : " + this.numberOfDoors);
+    }
+}
+
+var car1 = new Car("Yotota","Ryias","rouge",5);
+var car2 = new Car("Yotota","Risau","noire",3);
+car1.showCar();
+console.log("-----------------------");
+car2.showCar();
+
