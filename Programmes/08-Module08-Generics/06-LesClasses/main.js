c931b81a536359f4fe87791a6e0e23fca9de1b2b
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-02-16 23:53:34 +0100

    08-Module08-Generics-06-LesClasses

---
+++ Programmes/08-Module08-Generics/06-LesClasses/main.js
@@ -0,0 +1,23 @@
+const warrior1 = { name: "Milo", age: 18, class: "guerrier", tribe: "Orc vert" };
+const archer1 = { name: "Tya", age: 18, class: "archer", numberOfArrows: 50 };
+class Characters {
+    constructor() {
+        this.list = [];
+    }
+    addCharacter(perso) {
+        this.list.push(perso);
+    }
+    showCharacters() {
+        for (let element of this.list) {
+            console.log(element);
+        }
+    }
+}
+const listWarrior = new Characters();
+listWarrior.addCharacter(warrior1);
+console.log("Liste de guerriers :");
+listWarrior.showCharacters();
+const listArcher = new Characters();
+listArcher.addCharacter(archer1);
+console.log("Liste d'archers :");
+listArcher.showCharacters();
---
+++ Programmes/08-Module08-Generics/06-LesClasses/main.ts
@@ -0,0 +1,30 @@
+interface Character {name : string;age : number;}
+interface Warrior extends Character {class : "guerrier";tribe : string;}
+interface Archer extends Character {class : "archer";numberOfArrows : number;}
+
+const warrior1:Warrior = {name : "Milo",age : 18,class : "guerrier",tribe : "Orc vert"};
+const archer1:Archer = {name : "Tya",age : 18,class : "archer",numberOfArrows : 50};
+
+class Characters<T extends Character> {
+    private list:Array<T> = [];
+
+    public addCharacter(perso:T) {
+        this.list.push(perso);
+    }
+
+    public showCharacters() {
+        for(let element of this.list) {
+            console.log(element);
+        }
+    }
+}
+
+const listWarrior = new Characters<Warrior>();
+listWarrior.addCharacter(warrior1);
+console.log("Liste de guerriers :");
+listWarrior.showCharacters();
+
+const listArcher = new Characters<Archer>();
+listArcher.addCharacter(archer1);
+console.log("Liste d'archers :");
+listArcher.showCharacters();
